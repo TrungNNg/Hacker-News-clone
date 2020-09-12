f231240c51e55a47318b3da26f41ad111ba610b9
TrungNNg <tnguy384@mail.ccsf.edu>
2020-09-12 09:58:08 -0400

    create even handle function for input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const list = [
+  {
+    title: "React",
+    url: "https://react.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const List = () => 
+  list.map(item => (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+  ));
+
+
+function Search(){
+  return(
+    <div>
+      <h1>My Hacker Stories</h1>
+      <label htmlFor="Search">Search: </label>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const handleChange = event => {
+    console.log(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+      <label htmlFor="Search">Search: </label>
+      <input id='search' type='text' onChange={handleChange}/>
+
+      <hr />
+
+      <List />
     </div>
   );
 }
